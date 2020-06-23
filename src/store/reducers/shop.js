07707755c3f82fed2f625f8bb262c5a97c452064
fahadtufail007 <fahadtufail873@gmail.com>
2020-06-23 18:42:22 +0500

    modifies cart count

--- src/store/reducers/shop.js
+++ src/store/reducers/shop.js
@@ -3,7 +3,7 @@ import * as actionTypes from "../actions/actionTypes";
 const initialState = {
   cart: [],
   vat: 16, //vat in percentage
-  cartTotal: 0,
+  cartTotal: 1,
   orderSuccess: false,
   promoCode: [
     {
